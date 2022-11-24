870f031272755b60e93a0b1fc08bbd5f95b164f5
Sheikh sadi <you@example.com>
2022-11-24 15:07:37 +0600

    banner added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -14,16 +14,18 @@ const Main = () => {
                     <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                     <div className="drawer-content flex flex-col">
                          {/* <!-- Navbar --> */}
-                         <div className="w-full navbar bg-base-300">
+                         <div className="w-full navbar bg-base-300 ">
                               <div className="flex-none lg:hidden">
                                    <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                                    </label>
                               </div>
-                              <Link className="flex-1 px-2 mx-2">
-                                   <div className='h-8 w-8 mr-2'><img className='h-full w-full' src={logo} alt="" /></div>
-                                   <p className='text-xl font-serif'>SellDom</p>
-                              </Link>
+                              <div className="flex-1 px-2 mx-2">
+                                   <Link to='/' className='border-2 flex items-center'>
+                                        <div className='h-8 w-8 mr-2'><img className='h-full w-full' src={logo} alt="" /></div>
+                                        <p className='text-xl font-serif'>SellDom</p>
+                                   </Link>
+                              </div>
                               <div className="flex-none hidden lg:block">
                                    <ul className="menu menu-horizontal font-serif text-sm">
                                         {/* <!-- Navbar menu content here --> */}
@@ -37,7 +39,7 @@ const Main = () => {
                     </div>
                     <div className="drawer-side">
                          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-                         <ul className="menu p-4 w-80 bg-base-100 font-serif">                              
+                         <ul className="menu p-4 w-80 bg-base-100 font-serif">
                               {menuItems}
                          </ul>
                     </div>
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import banner from '../../../assets/banner.jpg'
+import './Banner.css';
 
 const Banner = () => {
      return (
-          <div className='mx-6'>
-               <img src={banner} alt="" />
+          <div className='banner h-[400px] max-w-5xl mx-auto flex justify-center items-center'>               
+               <div className='text-center'>
+                    <p className='text-5xl text-white font-serif'>SellDom</p>
+                    <p className='text-2xl text-white'>A PC Accesories Resale Platform</p>
+               </div>
           </div>
      );
 };
