362f209c85f8325b232d1a476cdab7cd614aa890
Sheikh sadi <you@example.com>
2022-11-25 01:00:40 +0600

    categories data loaded

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../Loading/Loading';
 
 const Categories = () => {
+     const {data: categories,isLoading} = useQuery({
+          queryKey: ['categories'],
+          queryFn: async()=>{
+               try{
+                    const res = await fetch('http://localhost:5000/categories');
+                    const data = await res.json()
+                    console.log(data)
+                    return data;
+               }
+               catch(error){
+                    console.error(error)
+               }
+          }
+     })
+
+     if(isLoading){
+          return <Loading></Loading>
+     }
      return (
-          <div>
-               this is categories page
+          <div className='max-w-5xl mx-auto my-5'>
+               <h1>categories {categories.length}</h1>
           </div>
      );
 };
---
+++ src/Pages/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+     return (
+          <div>
+               this is loading page
+          </div>
+     );
+};
+
+export default Loading;
